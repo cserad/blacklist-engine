26fbee60d7d8ee334aee6c4ad0e6948182ea6fb4
Adam Cser <cser.adam11@gmail.com>
2018-09-28 15:46:36 +0200

    Database made

--- blacklist-engineApp/database.cpp
+++ blacklist-engineApp/database.cpp
@@ -1,7 +1,29 @@
 #include "database.h"
 
-Database::Database()
+Database::Database(const QString &path)
+{
+    initDatabase(path);
+}
+
+bool Database::findHash(const QString &hash)
+{
+    QSqlQuery query;
+
+    query.prepare("SELECT hash FROM myhashes WHERE hash = (:hash);");
+    query.bindValue(":hash", hash);
+    query.exec();
+
+    return query.first();
+}
+
+void Database::initDatabase(const QString &path)
 {
     db = QSqlDatabase::addDatabase("QSQLITE");
-    db.setDatabaseName("hashes");
+    db.setDatabaseName(path);
+
+    if (!db.open()) {
+        qDebug() << "Error: cannot connect to database";
+    } else {
+        qDebug() << "ok";
+    }
 }
